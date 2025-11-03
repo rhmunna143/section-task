a7ee4a813df4fc14f6e54ef93705c1d69d934a53
rhmunna143 <rhmunna19@gmail.com>
2025-11-03 11:04:09 +0600

    first commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            <h1 className="">Welcome to My Website</h1>
+        </div>
+    );
+};
+
+export default Heading;
